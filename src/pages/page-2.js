f6a13200a4b32b9f8e4bb4fea6bdd7b0ec09ce46
Naveen Shivnani <naveenshivnani586@gmail.com>
2020-10-24 13:29:57 +0530

    Updated page2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2! you won't find anything here, click the link below to go to the main page!</p>
     <p><a href="https://in-the-galaxy.netlify.app/">Wanna go to Space with us!</a></p>
   </Layout>
 )
